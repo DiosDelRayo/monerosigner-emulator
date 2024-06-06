6fcf716ee6fdfae9fff4645d17911edd1d7cf3fe
who asks? <user@localhost>
2024-06-06 04:39:42 -0600

    try to solve the win32 stuff

---
+++ dependencies.ps1
@@ -0,0 +1,19 @@
+$MONERO_SIGNER_DIR = "..\MoneroSigner"
+$EMULATOR_DIR = "."
+$DIST_DIR = "dist"
+$BUILD_DIR = "build"
+
+echo Quickstart
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install python -y
+choco install git -y
+choco install vim -y
+
+mkdir "C:\monerosigner"
+cd "C:\monerosigner"
+git clone https://github.com/DiosDelRayo/MoneroSigner.git
+git clone https://github.com/DiosDelRayo/monerosigner-emulator.git
