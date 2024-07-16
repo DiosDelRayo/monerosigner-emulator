bbbafca1330de776af48c98ecb5175dc5f084384
who asks? <user@localhost>
2024-07-16 10:21:16 -0600

    fix camera rotation on emulator

--- src/xmrsigner/__main__.py
+++ src/xmrsigner/__main__.py
@@ -2,6 +2,13 @@ from xmrsigner.helpers import wallet as wallet_controller
 wallet_controller.WALLET_DAEMON_PATH = '/home/thor/monero-gui-v0.18.3.3/extras/monero-wallet-rpc'
 wallet_controller.PIDFILE_BASE_PATH = '/tmp/monero-wallet-rpc'
 
+from xmrsigner.models.settings import Settings
+from xmrsigner.models.settings_definition import SettingsConstants, SettingsDefinition
+
+settings = Settings.get_instance()
+settings.set_value(SettingsConstants.SETTING__CAMERA_ROTATION, SettingsConstants.CAMERA_ROTATION__270)  # fix camera rotation for emulator
+
+
 from xmrsigner.controller import Controller
 # Get the one and only Controller instance and start our main loop
 Controller.get_instance().start()
