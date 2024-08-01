cb89f8492f60287ed39044770bf8ac6ab44ef089
who asks? <user@localhost>
2024-07-31 23:10:11 -0600

    automatically find the right paths, add monero-wallet-rpc binary into package, hande config file and screenshots

--- src/xmrsigner/__main__.py
+++ src/xmrsigner/__main__.py
@@ -1,12 +1,37 @@
 from xmrsigner.helpers import wallet as wallet_controller
+import xmrsigner.emulator.desktopDisplay as desktopDisplay
 from os import path
+from random import randint
+import sys
 
-wallet_controller.WALLET_DAEMON_PATH = \
-        path.join(path.dirname(path.abspath(__file__)), 'monero-wallet-rpc') \
-        if path.basename(path.dirname(path.abspath(__package__))) != 'emulator' else \
-        path.join(path.dirname(path.dirname(path.abspath(__package__))), 'monero-wallet-rpc')
-wallet_controller.PIDFILE_BASE_PATH = '/tmp/monero-wallet-rpc'
-wallet_controller.WALLET_RPC_PORT_OFFSET = 100
+
+WALLET_RPC_EXECUTABLE = 'monero-wallet-rpc'
+
+
+def is_executable() -> bool:
+    return getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+
+def get_path() -> str:
+    if is_executable():
+        return path.dirname(path.abspath(__file__))  # same path as the extracted script
+    return path.dirname(path.dirname(path.abspath(__package__)))  # on directory up of the module
+
+def get_file_path(file: str) -> str:
+    return path.join(get_path(), file)
+
+def get_executable_file_path() -> str:
+    if is_executable():
+        return path.dirname(path.abspath(sys.executable))  # same path as the extracted script
+    return path.dirname(path.dirname(path.abspath(__package__)))  # on directory up of the module
+
+wallet_controller.WALLET_DAEMON_PATH = get_file_path(WALLET_RPC_EXECUTABLE)
+wallet_controller.PIDFILE_BASE_PATH = f'/tmp/{WALLET_RPC_EXECUTABLE}'
+wallet_controller.WALLET_RPC_PORT_OFFSET = 100 + randint(0, 99)
+desktopDisplay.CONFIG_FILE = path.join(get_executable_file_path(), 'xmrsigner-emulator.yml')
+desktopDisplay.SCREENSHOT_PATH = get_executable_file_path()
+
+print(f"wallet rpc: {wallet_controller.WALLET_DAEMON_PATH}: {'found' if path.exists(wallet_controller.WALLET_DAEMON_PATH) else 'not found'}")
+print(f'temp dir: {wallet_controller.PIDFILE_BASE_PATH}')
 
 from xmrsigner.models.settings import Settings
 from xmrsigner.models.settings_definition import SettingsConstants, SettingsDefinition
